1f02faf17f3f903d842e35aac2bea71d63da2830
宋浩茹 <amber.sung@kerrytj.com>
2020-11-06 16:24:58 +0800

    add project

---
+++ Backend_Structure/Backend_Structure/Api/DemoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Structure.Api
+{
+    /// <summary>
+    /// 設定API路由
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+
+    }
+}
